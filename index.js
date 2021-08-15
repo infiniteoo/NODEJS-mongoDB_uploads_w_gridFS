e4494ca533dddcc32e298f3d0fc184c4b9a1c1ea
Troy Dorman <troydorman@gmail.com>
2021-08-15 12:14:41 -0600

    integrated bootstrap CDN links into our markup

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-// require dotenv
 require("dotenv").config();
-
 const express = require("express");
 const app = express();
+
 app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
   res.render("index");
 });

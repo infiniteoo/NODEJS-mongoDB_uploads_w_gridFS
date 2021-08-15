4c013947c01803dc27686fb19c434cbe8cd845ac
Troy Dorman <troydorman@gmail.com>
2021-08-15 12:08:36 -0600

    installed dot env and put our mongodb connection string in .env. set up basic express server

---
+++ index.js
@@ -0,0 +1,13 @@
+// require dotenv
+require("dotenv").config();
+
+const express = require('express');
+const app = express();
+app.set('view engine', ejs);
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+const port = 5000;
+
+app.listen(port, () => {console.log(`Listening on port ${port}`);});

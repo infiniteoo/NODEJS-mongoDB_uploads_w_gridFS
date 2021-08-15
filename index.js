0f121bc169419709b720f8bff7a101a18b6023c6
Troy Dorman <troydorman@gmail.com>
2021-08-15 12:35:03 -0600

    created storage engine

--- index.js
+++ index.js
@@ -18,6 +18,43 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+// Mongo URI from environment variable
+const mongoURI = process.env.MONGODB;
+
+// create mongo connection
+const conn = mongoose.createConnection(mongoURI);
+
+// init gfs
+let gfs;
+
+conn.once("open", () => {
+  // init stream
+  gfs = Grid(conn.db, mongoose.mongo);
+  gfs.collection("uploads");
+});
+
+// Create storage engine
+const storage = new GridFsStorage({
+  url: mongoURI,
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, (err, buf) => {
+        if (err) {
+          return reject(err);
+        }
+        const filename = buf.toString("hex") + path.extname(file.originalname);
+        const fileInfo = {
+          filename: filename,
+          bucketName: "uploads",
+        };
+        resolve(fileInfo);
+      });
+    });
+  },
+});
+
+const upload = multer({ storage });
+
 const port = 5000;
 
 app.listen(port, () => {

7196abd0f57b30d24a91c6e2ba9b58bc9423260e
Troy Dorman <troydorman@gmail.com>
2021-08-15 12:12:50 -0600

    added readme and put some info in there

---
+++ README.md
@@ -0,0 +1,7 @@
+# NODE.JS - MongoDB File Uploads w/GridFS
+
+### About
+
+In this project we create an application that can upload files of all types to a MongoDB database with GridFS using Node.js, Multer, the GridFS storage engine and gridfs-stream, along with the EJS template engine.
+
+![examplegif](./example.gif)
--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 // require dotenv
 require("dotenv").config();
 
-const express = require('express');
+const express = require("express");
 const app = express();
-app.set('view engine', ejs);
-app.get('/', (req, res) => {
-    res.render('index');
+app.set("view engine", "ejs");
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
 const port = 5000;
 
-app.listen(port, () => {console.log(`Listening on port ${port}`);});
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});

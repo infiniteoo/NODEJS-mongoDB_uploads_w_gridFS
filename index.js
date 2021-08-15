6b2b709ba96a8808e2dbdae6aaac693d2879ef38
Troy Dorman <troydorman@gmail.com>
2021-08-15 16:10:06 -0600

    created /image/:filename GET route to display an image from our database

--- index.js
+++ index.js
@@ -77,9 +77,44 @@ app.get("/files", (req, res) => {
         message: "No files found",
       });
     }
-
     // files exist
     return res.json(files);
+  });
+});
+
+// @route: GET /files/:filename
+// @desc: display single file in JSON
+app.get("/files/:filename", (req, res) => {
+  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+    if (!file || file.length === 0) {
+      return res.status(404).json({
+        message: "No file found",
+      });
+    }
+    // file exists
+    return res.json(file);
+  });
+});
+
+// @route: GET /image/:filename
+// @desc: display image
+app.get("/image/:filename", (req, res) => {
+  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+    if (!file || file.length === 0) {
+      return res.status(404).json({
+        message: "No file found",
+      });
+    }
+    // check if file type is image
+    if (file.contentType === "image/jpeg" || file.contentType === "img/png") {
+      // read output to browser
+      const readstream = gfs.createReadStream(file.filename);
+      readstream.pipe(res);
+    } else {
+      res.status(404).json({
+        err: "not an image",
+      });
+    }
   });
 });
 

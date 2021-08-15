d8745aaf05b26f864536218fec8744bd2afabf2e
Troy Dorman <troydorman@gmail.com>
2021-08-15 16:30:01 -0600

    created DELETE route at /files/:id to remove a file from the database. tested successfully

--- index.js
+++ index.js
@@ -136,6 +136,18 @@ app.get("/image/:filename", (req, res) => {
   });
 });
 
+// @route DELETE /files/:id
+// @desc Delete file
+app.delete("/files/:id", (req, res) => {
+  gfs.remove({ _id: req.params.id, root: "uploads" }, (err, gridStore) => {
+    if (err) {
+      return res.status(404).json({ err: err });
+    }
+
+    res.redirect("/");
+  });
+});
+
 const port = 5000;
 
 app.listen(port, () => {
